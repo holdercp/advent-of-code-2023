87670f87146b49871f26553b582043857c7de825
Chris Holder <chris@chrisholder.dev>
2023-12-02 08:49:15 -0600

    Solve day 2, part 1

--- solutions/02/index.ts
+++ solutions/02/index.ts
@@ -4,9 +4,58 @@ import * as two from "./part-2";
 console.log(`Part 1: `, await one.solve());
 console.log(`Part 2: `, await two.solve());
 
+const colors = ["red", "green", "blue"] as const;
+
+type Color = (typeof colors)[number];
+
+export type Reveal = {
+  red?: number;
+  green?: number;
+  blue?: number;
+};
+
+export type Games = {
+  [k: string]: Reveal[];
+};
+
+export function isColor(c: string): c is Color {
+  return c in colors;
+}
+
+function parseColorPair(pair: string): [Color, number] {
+  const [count, color] = pair.split(" ");
+  const countInt = parseInt(count, 10);
+
+  if (!isColor(color)) throw new Error(`color is not valid: ${color}`);
+  if (isNaN(countInt)) throw new Error(`count is not valid ${count}`);
+
+  return [color, countInt];
+}
+
 export async function parseInput(example = false) {
   const file = `${import.meta.dir}/input${example ? "-example" : ""}.txt`;
   const input = await Bun.file(file).text();
+  const games = input
+    .trim()
+    .split("\n")
+    .reduce<Games>((g, l, i) => {
+      const [, revealsRaw] = l.split(": ");
+      const reveals = revealsRaw.split("; ");
 
-  return input.trim().split("\n");
+      const gameReveals = reveals.map((r) => {
+        const colors = r.split(", ");
+        const colorCounts = colors.reduce<Reveal>((o, c) => {
+          const [color, count] = parseColorPair(c);
+          return { ...o, [color]: count };
+        }, {});
+
+        return colorCounts;
+      });
+
+      g[`${i + 1}`] = gameReveals;
+
+      return g;
+    }, {});
+
+  return games;
 }
--- solutions/02/part-1.ts
+++ solutions/02/part-1.ts
@@ -1,7 +1,27 @@
-import { parseInput } from ".";
+import { Games, Reveal, isColor, parseInput } from ".";
+
+const LIMITS = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+function isPossible(gameTuple: [keyof Games, Reveal[]]) {
+  return gameTuple[1].every((r) =>
+    Object.entries(r).every(([color, count]) => {
+      if (!isColor(color)) throw new Error(`color is not valid: ${color}`);
+
+      return count <= LIMITS[color];
+    }),
+  );
+}
 
 export async function solve() {
-  const input = await parseInput();
+  const games = await parseInput();
 
-  return "Not implemented.";
+  const possibleGameIds = Object.entries(games)
+    .filter(isPossible)
+    .map(([id]) => parseInt(id, 10));
+
+  return possibleGameIds.reduce((sum, id) => sum + id);
 }
