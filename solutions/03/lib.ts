346c243b635daf5b0bfba9851112af797f37e7ee
holdercp <chris@chrisholder.dev>
2023-12-03 00:41:22 -0600

    solve day 3, part 1

--- solutions/03/lib.ts
+++ solutions/03/lib.ts
@@ -2,6 +2,8 @@ import { readInput } from "../../lib/helpers";
 
 export async function parseInput() {
   const input = await readInput(import.meta.dir);
+  const lineLen = input.indexOf("\n");
+  const schematic = input.replaceAll("\n", "").trim();
 
-  return input;
+  return { schematic, lineLen };
 }
--- solutions/03/part-1.ts
+++ solutions/03/part-1.ts
@@ -1,7 +1,70 @@
+import { stringIsInt } from "../../lib/utils";
 import { parseInput } from "./lib";
 
-export async function solve() {
-  const input = await parseInput();
+function getAdj(i: number, len: number) {
+  return [
+    i - (len + 1),
+    i - len,
+    i - (len - 1),
+    i - 1,
+    i + 1,
+    i + (len + 1),
+    i + len,
+    i + (len - 1),
+  ];
+}
 
-  return "Not implemented.";
+export async function solve() {
+  const { schematic, lineLen } = await parseInput();
+
+  let sum = 0;
+  const posNumMap: { [k: string]: { id: number; value: number } } = {};
+
+  let activeNumId = 1;
+  let activeNum = "";
+  let pos = [];
+  for (let i = 0; i < schematic.length; i++) {
+    const c = schematic[i];
+
+    if (stringIsInt(c) && i % lineLen !== 0) {
+      activeNum += c;
+      pos.push(i);
+      continue;
+    }
+
+    if (activeNum) {
+      const num = {
+        id: activeNumId,
+        value: parseInt(activeNum, 10),
+      };
+      pos.forEach((p) => {
+        posNumMap[p] = num;
+      });
+      activeNum = "";
+      activeNumId += 1;
+      pos = [];
+    }
+
+    if (stringIsInt(c)) {
+      activeNum += c;
+      pos.push(i);
+    }
+  }
+
+  const seenIds: number[] = [];
+  for (let i = 0; i < schematic.length; i++) {
+    const c = schematic[i];
+
+    if (c !== "." && !stringIsInt(c)) {
+      const adj = getAdj(i, lineLen);
+      adj.forEach((a) => {
+        if (posNumMap[a] && !seenIds.includes(posNumMap[a].id)) {
+          sum += posNumMap[a].value;
+          seenIds.push(posNumMap[a].id);
+        }
+      });
+    }
+  }
+
+  return sum;
 }
