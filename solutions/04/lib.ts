1b3e6b81416304020379778308d2ad8d6cefe47a
Chris Holder <chris@chrisholder.dev>
2023-12-04 12:53:30 -0600

    Solve day 4, part 1

--- solutions/04/lib.ts
+++ solutions/04/lib.ts
@@ -1,7 +1,22 @@
 import { readInput } from "../../lib/helpers";
 
+function parseNum(numStr: string) {
+  return numStr
+    .split(" ")
+    .filter((n) => n)
+    .map(Number);
+}
+
 export async function parseInput() {
   const input = await readInput(import.meta.dir);
+  const cards = input
+    .split("\n")
+    .map((c) => c.split(": ")[1])
+    .map((n) => n.split(" | "))
+    .map(([wNums, nums]) => ({
+      winners: parseNum(wNums),
+      numbers: parseNum(nums),
+    }));
 
-  return input;
+  return cards;
 }
--- solutions/04/part-1.ts
+++ solutions/04/part-1.ts
@@ -1,7 +1,20 @@
 import { parseInput } from "./lib";
 
-export async function solve() {
-  const input = await parseInput();
+function findDuplicates(a: number[], b: number[]) {
+  const d: number[] = [];
+  b.forEach((n) => {
+    if (a.includes(n)) d.push(n);
+  });
+  return d;
+}
 
-  return "Not implemented.";
+export async function solve() {
+  const cards = await parseInput();
+
+  const worth = cards.reduce((sum, c) => {
+    const matches = findDuplicates(c.winners, c.numbers).length;
+    return matches ? sum + 1 * Math.pow(2, matches - 1) : sum;
+  }, 0);
+
+  return worth;
 }
