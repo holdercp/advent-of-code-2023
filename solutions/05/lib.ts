15359626f9f72215ee29a6dce85f4c50e6731831
Chris Holder <chris@chrisholder.dev>
2023-12-05 15:32:19 -0600

    Solve day 5, part 1

--- solutions/05/lib.ts
+++ solutions/05/lib.ts
@@ -1,7 +1,77 @@
 import { readInput } from "../../lib/helpers";
 
+const mapNames = [
+  "seed-to-soil",
+  "soil-to-fertilizer",
+  "fertilizer-to-water",
+  "water-to-light",
+  "light-to-temperature",
+  "temperature-to-humidity",
+  "humidity-to-location",
+] as const;
+
+type MapName = (typeof mapNames)[number];
+
+type Maps = {
+  [k in MapName]: DestMap[];
+};
+
+export type DestMap = {
+  src: {
+    start: number;
+    end: number;
+  };
+  dest: {
+    start: number;
+    end: number;
+  };
+};
+
+export function isMapName(n: string): n is MapName {
+  return mapNames.includes(n as MapName);
+}
+
+function parseMapsMapper(m: string) {
+  const [titleRaw, ...data] = m.split("\n");
+
+  const [title] = titleRaw.split(" map:");
+
+  const parsedMap = data
+    .map((d) => d.split(" ").map(Number))
+    .map((r) => {
+      const [destStart, srcStart, len] = r;
+      return {
+        src: {
+          start: srcStart,
+          end: srcStart + (len - 1),
+        },
+        dest: {
+          start: destStart,
+          end: destStart + (len - 1),
+        },
+      };
+    });
+
+  return { title, parsedMap };
+}
+
+function parseSeeds(s: string) {
+  return s.split(": ")[1].split(" ").map(Number);
+}
+
 export async function parseInput() {
   const input = await readInput(import.meta.dir);
+  const [seedsRaw, ...mapsRaw] = input.split("\n\n");
 
-  return input;
+  const seeds = parseSeeds(seedsRaw);
+  const maps = mapsRaw
+    .map(parseMapsMapper)
+    .reduce((acc, { title, parsedMap }) => {
+      if (!isMapName(title)) throw new Error("bad title");
+
+      acc[title] = parsedMap;
+      return acc;
+    }, {} as Maps);
+
+  return { seeds, maps };
 }
--- solutions/05/part-1.ts
+++ solutions/05/part-1.ts
@@ -1,7 +1,29 @@
-import { parseInput } from "./lib";
+import { DestMap, parseInput } from "./lib";
+
+function mapBuilder(mapData: DestMap[]) {
+  return function mapper(source: number) {
+    const foundMap = mapData.find(
+      (d) => d.src.start <= source && d.src.end >= source,
+    );
+
+    if (!foundMap) return source;
+
+    const offset = source - foundMap.src.start;
+    return foundMap.dest.start + offset;
+  };
+}
 
 export async function solve() {
-  const input = await parseInput();
+  const { seeds, maps } = await parseInput();
 
-  return "Not implemented.";
+  const locations = seeds
+    .map(mapBuilder(maps["seed-to-soil"]))
+    .map(mapBuilder(maps["soil-to-fertilizer"]))
+    .map(mapBuilder(maps["fertilizer-to-water"]))
+    .map(mapBuilder(maps["water-to-light"]))
+    .map(mapBuilder(maps["light-to-temperature"]))
+    .map(mapBuilder(maps["temperature-to-humidity"]))
+    .map(mapBuilder(maps["humidity-to-location"]));
+
+  return Math.min(...locations);
 }
