20b34cd7f1e0d04241f3d490ac35bf7073c95029
Chris Holder <chris@chrisholder.dev>
2023-12-06 13:43:55 -0600

    Solve day 6, part 1

--- solutions/06/lib.ts
+++ solutions/06/lib.ts
@@ -1,7 +1,19 @@
 import { readInput } from "../../lib/helpers";
 
-export async function parseInput() {
-  const input = await readInput(import.meta.dir);
+function parseRaw(s: string, delimiter = "") {
+  return s
+    .split(delimiter)[1]
+    .trim()
+    .split(" ")
+    .filter((t) => t)
+    .map(Number);
+}
 
-  return input;
+export async function parseInput() {
+  const [timeRaw, disRaw] = (await readInput(import.meta.dir)).split("\n");
+
+  const times = parseRaw(timeRaw, "Time: ");
+  const distances = parseRaw(disRaw, "Distance: ");
+
+  return { times, distances };
 }
--- solutions/06/part-1.ts
+++ solutions/06/part-1.ts
@@ -1,7 +1,26 @@
 import { parseInput } from "./lib";
 
 export async function solve() {
-  const input = await parseInput();
+  const { times, distances } = await parseInput();
 
-  return "Not implemented.";
+  const winners = [];
+  for (let i = 0; i < times.length; i++) {
+    const time = times[i];
+    const record = distances[i];
+
+    let winningHolds = 0;
+    for (let holdTime = 0; holdTime < time; holdTime++) {
+      const speed = holdTime;
+
+      const timeRemaining = time - holdTime;
+      const distanceTraveled = timeRemaining * speed;
+
+      if (distanceTraveled > record) {
+        winningHolds += 1;
+      }
+    }
+    winners.push(winningHolds);
+  }
+
+  return winners.reduce((prod, w) => prod * w);
 }
