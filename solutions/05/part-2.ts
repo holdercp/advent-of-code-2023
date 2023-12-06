66fb13a8dcd74410ca61b40ced4b0427f2688d45
holdercp <chris@chrisholder.dev>
2023-12-05 22:17:08 -0600

    Getting closer

--- solutions/05/part-2.ts
+++ solutions/05/part-2.ts
@@ -1,82 +1,85 @@
 import { DestMap, parseInput } from "./lib";
 
 function mapBuilder(mapData: DestMap[]) {
+  const sortedMaps = mapData.toSorted((a, b) => {
+    if (a.src.start < b.src.start) return -1;
+    if (a.src.start > b.src.start) return 1;
+    return 0;
+  });
+
   return function mapper(pair: number[]) {
-    const remainingPairs = [[...pair]];
-    const unmatchedPairs = [];
+    let [pStart, pEnd] = pair;
     const pairs = [];
+    let unmatched: number[] = [];
 
-    while (remainingPairs.length > 0) {
-      const currentPair = remainingPairs.pop();
-      if (!currentPair) throw new Error("bad pair");
+    for (let i = 0; i < sortedMaps.length; i++) {
+      const m = sortedMaps[i];
 
-      const [pStart, pEnd] = currentPair;
-
-      let matched = false;
-      for (let i = 0; i < mapData.length; i++) {
-        const m = mapData[i];
-
-        // Outside bounds
-        if (pEnd < m.src.start || pStart > m.src.end) {
-          continue;
-        }
-
-        // Range is contained completely in bounds
-        if (pStart >= m.src.start && pEnd <= m.src.end) {
-          const sOffset = pStart - m.src.start;
-          const eOffset = m.src.end - pEnd;
-
-          const dStart = m.dest.start + sOffset;
-          const dEnd = m.dest.end - eOffset;
-
-          pairs.push([dStart, dEnd]);
-          matched = true;
-          continue;
-
-          // Range overlaps on lower bounds
-        } else if (pStart < m.src.start && pEnd <= m.src.end) {
-          const eOffset = m.src.end - pEnd;
-
-          const dStart = m.dest.start;
-          const dEnd = m.dest.end - eOffset;
-
-          pairs.push([dStart, dEnd]);
-
-          remainingPairs.push([pStart, m.src.start - 1]);
-          matched = true;
-          continue;
-
-          // Range overlaps on upper bounds
-        } else if (pStart >= m.src.start && pEnd > m.src.end) {
-          const sOffset = pStart - m.src.start;
-
-          const dStart = m.dest.start - sOffset;
-          const dEnd = m.dest.end;
-
-          pairs.push([dStart, dEnd]);
-
-          remainingPairs.push([m.src.end + 1, pEnd]);
-          matched = true;
-          continue;
-
-          // Range contains bounds
-        } else if (pStart < m.src.start && pEnd > m.src.end) {
-          pairs.push([m.dest.start, m.dest.end]);
-
-          remainingPairs.push([pStart, m.src.start - 1], [m.src.end + 1, pEnd]);
-          matched = true;
-          continue;
-        } else {
-          throw new Error("shouldn't get here");
-        }
+      // Outside lower bounds
+      if (pEnd < m.src.start) {
+        unmatched = [pStart, pEnd];
+        break;
       }
 
-      if (!matched) {
-        unmatchedPairs.push(currentPair);
+      // Range is completely contained in bounds
+      if (pStart >= m.src.start && pEnd <= m.src.end) {
+        const sOffset = pStart - m.src.start;
+        const eOffset = m.src.end - pEnd;
+
+        const dStart = m.dest.start + sOffset;
+        const dEnd = m.dest.end - eOffset;
+
+        pairs.push([dStart, dEnd]);
+        break;
+      }
+
+      // Outside upper bounds
+      if (pStart > m.src.end) {
+        if (i === sortedMaps.length - 1) {
+          unmatched = [pStart, pEnd];
+        }
+        continue;
+      }
+
+      // Overlaps upper bounds
+      if (pStart >= m.src.start && pEnd > m.src.end) {
+        const sOffset = pStart - m.src.start;
+
+        const dStart = m.dest.start - sOffset;
+        const dEnd = m.dest.end;
+
+        pairs.push([dStart, dEnd]);
+
+        pStart = m.src.end + 1;
+        continue;
+      }
+
+      // Overlaps lower bounds
+      if (pStart < m.src.start && pEnd <= m.src.end) {
+        const eOffset = m.src.end - pEnd;
+
+        const dStart = m.dest.start;
+        const dEnd = m.dest.end - eOffset;
+
+        pairs.push([dStart, dEnd]);
+
+        pEnd = m.src.start - 1;
+        continue;
+      }
+
+      if (pStart < m.src.start && pEnd > m.src.end) {
+        pairs.push([m.dest.start, m.dest.end]);
+
+        pStart = m.src.end + 1;
+        continue;
       }
     }
 
-    return [...pairs, ...unmatchedPairs];
+    if (unmatched.length > 0) {
+      pairs.push(unmatched);
+    }
+
+    return pairs;
   };
 }
 
@@ -91,6 +94,8 @@ export async function solve() {
     .flatMap(mapBuilder(maps["light-to-temperature"]))
     .flatMap(mapBuilder(maps["temperature-to-humidity"]))
     .flatMap(mapBuilder(maps["humidity-to-location"]));
+
+  console.log(locations);
 
   return Math.min(...locations.map(([ls]) => ls));
 }
