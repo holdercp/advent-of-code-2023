3d4e205dc44e0ab9af66d43fbd970368325bd91e
Chris Holder <chris@chrisholder.dev>
2023-12-07 10:18:47 -0600

    Solve day 7, part 1

--- solutions/07/lib.ts
+++ solutions/07/lib.ts
@@ -1,7 +1,120 @@
 import { readInput } from "../../lib/helpers";
 
-export async function parseInput() {
-  const input = await readInput(import.meta.dir);
+function getCardCounts(h: Hand) {
+  const { cards } = h;
 
-  return input;
+  return cards.reduce(
+    (counts, card) => {
+      counts[card] = (counts[card] ?? 0) + 1;
+
+      return counts;
+    },
+    {} as Record<Card, number>,
+  );
+}
+
+function cardMapper(card: string) {
+  switch (card) {
+    case "A":
+      return Card.A;
+    case "K":
+      return Card.K;
+    case "Q":
+      return Card.Q;
+    case "J":
+      return Card.J;
+    case "T":
+      return Card.T;
+    case "9":
+      return Card.Nine;
+    case "8":
+      return Card.Eight;
+    case "7":
+      return Card.Seven;
+    case "6":
+      return Card.Six;
+    case "5":
+      return Card.Five;
+    case "4":
+      return Card.Four;
+    case "3":
+      return Card.Three;
+    case "2":
+      return Card.Two;
+    default:
+      throw new Error("unknown card");
+  }
+}
+
+enum Card {
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  T,
+  J,
+  Q,
+  K,
+  A,
+}
+
+export type Hand = {
+  cards: Card[];
+  bid: number;
+};
+
+export enum HandType {
+  HighCard,
+  OnePair,
+  TwoPair,
+  ThreeOfAKind,
+  FullHouse,
+  FourOfAKind,
+  FiveOfAKind,
+}
+
+export function getHandType(h: Hand): HandType {
+  const cardCounts = getCardCounts(h);
+  const counts = Object.values(cardCounts);
+
+  if (counts.length === 1) {
+    return HandType.FiveOfAKind;
+  }
+
+  if (counts.some((c) => c === 4)) {
+    return HandType.FourOfAKind;
+  }
+
+  if (counts.length === 2 && counts.some((c) => c === 3)) {
+    return HandType.FullHouse;
+  }
+
+  if (counts.some((c) => c === 3)) {
+    return HandType.ThreeOfAKind;
+  }
+
+  if (counts.length === 3 && counts.some((c) => c >= 2)) {
+    return HandType.TwoPair;
+  }
+
+  if (counts.length === 4 && counts.some((c) => c >= 2)) {
+    return HandType.OnePair;
+  }
+
+  return HandType.HighCard;
+}
+
+export async function parseInput() {
+  const hands = (await readInput(import.meta.dir))
+    .split("\n")
+    .map((h): Hand => {
+      const [cards, bid] = h.split(" ");
+      return { cards: cards.split("").map(cardMapper), bid: Number(bid) };
+    });
+
+  return hands;
 }
--- solutions/07/part-1.ts
+++ solutions/07/part-1.ts
@@ -1,7 +1,52 @@
-import { parseInput } from "./lib";
+import { Hand, getHandType, parseInput } from "./lib";
+
+function findStronger(h1: Hand, h2: Hand): Hand | null {
+  const { cards: h1Cards } = h1;
+  const { cards: h2Cards } = h2;
+
+  for (let i = 0; i < h1Cards.length; i++) {
+    const h1Card = h1Cards[i];
+    const h2Card = h2Cards[i];
+
+    if (h1Card > h2Card) {
+      return h1;
+    }
+
+    if (h1Card < h2Card) {
+      return h2;
+    }
+  }
+
+  return null;
+}
+
+function handSorter(h1: Hand, h2: Hand) {
+  const h1Type = getHandType(h1);
+  const h2Type = getHandType(h2);
+
+  const result = h1Type - h2Type;
+
+  // Hands are same type so compare individual cards
+  if (result === 0) {
+    const stronger = findStronger(h1, h2);
+    if (stronger === h1) {
+      return 1;
+    }
+
+    if (stronger === h2) {
+      return -1;
+    }
+
+    return 0;
+  }
+
+  return result;
+}
 
 export async function solve() {
-  const input = await parseInput();
+  const hands = await parseInput();
 
-  return "Not implemented.";
+  const sortedHands = hands.sort(handSorter);
+
+  return sortedHands.reduce((total, { bid }, i) => total + bid * (i + 1), 0);
 }
