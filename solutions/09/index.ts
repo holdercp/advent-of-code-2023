a709032de232b13a92b4866251f8c957b4e86d34
holdercp <chris@chrisholder.dev>
2023-12-09 08:54:05 -0600

    Scaffold day 9

---
+++ solutions/09/index.ts
@@ -0,0 +1,5 @@
+import * as one from "./part-1";
+import * as two from "./part-2";
+
+console.log(`Part 1: `, await one.solve());
+console.log(`Part 2: `, await two.solve());
---
+++ solutions/09/lib.ts
@@ -0,0 +1,7 @@
+import { readInput } from "../../lib/helpers";
+
+export async function parseInput() {
+  const input = await readInput(import.meta.dir);
+
+  return input;
+}
