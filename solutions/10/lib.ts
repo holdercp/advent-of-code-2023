300d86275ff54580f10aec3c9730123d5f66ca6e
holdercp <chris@chrisholder.dev>
2023-12-10 11:42:23 -0600

    Solve day 10, part 1

--- solutions/10/lib.ts
+++ solutions/10/lib.ts
@@ -1,7 +1,109 @@
 import { readInput } from "../../lib/helpers";
 
-export async function parseInput() {
-  const input = await readInput(import.meta.dir);
+export type Graph = {
+  [id: string]: Connection;
+};
 
-  return input;
+type Connection = { [id: string]: number };
+
+function getConnections(
+  pipe: string,
+  pos: number,
+  len: number,
+  map: string,
+): Connection {
+  if (pipe === "|") {
+    return {
+      [`${pos - len}`]: 1,
+      [`${pos + len}`]: 1,
+    };
+  }
+
+  if (pipe === "-") {
+    return {
+      [`${pos - 1}`]: 1,
+      [`${pos + 1}`]: 1,
+    };
+  }
+
+  if (pipe === "L") {
+    return {
+      [`${pos - len}`]: 1,
+      [`${pos + 1}`]: 1,
+    };
+  }
+
+  if (pipe === "J") {
+    return {
+      [`${pos - len}`]: 1,
+      [`${pos - 1}`]: 1,
+    };
+  }
+
+  if (pipe === "7") {
+    return {
+      [`${pos - 1}`]: 1,
+      [`${pos + len}`]: 1,
+    };
+  }
+
+  if (pipe === "F") {
+    return {
+      [`${pos + 1}`]: 1,
+      [`${pos + len}`]: 1,
+    };
+  }
+
+  if (pipe === "S") {
+    const n = pos - len;
+    const s = pos + len;
+    const e = pos + 1;
+    const w = pos - 1;
+
+    const connection: Connection = {};
+
+    if (map[n] === "|" || map[n] === "F" || map[n] === "7") {
+      connection[`${n}`] = 1;
+    }
+
+    if (map[s] === "|" || map[s] === "J" || map[s] === "L") {
+      connection[`${s}`] = 1;
+    }
+
+    if (map[e] === "-" || map[e] === "7" || map[e] === "J") {
+      connection[`${e}`] = 1;
+    }
+
+    if (map[w] === "-" || map[w] === "L" || map[w] === "F") {
+      connection[`${w}`] = 1;
+    }
+
+    return connection;
+  }
+
+  throw new Error(`bad pipe: ${pipe}`);
+}
+
+function createGraph(s: string, len: number): Graph {
+  const g: Graph = {};
+
+  for (let i = 0; i < s.length; i++) {
+    const el = s[i];
+
+    if (el === ".") continue;
+
+    const connections = getConnections(el, i, len, s);
+    g[`${i}`] = connections;
+  }
+
+  return g;
+}
+
+export async function parseInput(): Promise<{ graph: Graph; start: string }> {
+  const input = await readInput(import.meta.dir);
+  const rowLen = input.indexOf("\n");
+  const pipes = input.replaceAll("\n", "");
+  const start = `${pipes.indexOf("S")}`;
+
+  return { graph: createGraph(pipes, rowLen), start };
 }
--- solutions/10/part-1.ts
+++ solutions/10/part-1.ts
@@ -1,7 +1,34 @@
-import { parseInput } from "./lib";
+import { Graph, parseInput } from "./lib";
+
+function followPipe(graph: Graph, start: string): number {
+  let current = start;
+  let prev: string | null = null;
+  let moves = 0;
+  let done = false;
+  while (!done) {
+    const connections = graph[current];
+    const next = !prev
+      ? Object.keys(connections)[0]
+      : Object.keys(connections).find((c) => c !== prev);
+
+    if (!next) throw new Error("bad connection");
+
+    prev = current;
+    moves += connections[next];
+
+    if (next === start) {
+      done = true;
+      break;
+    }
+
+    current = next;
+  }
+
+  return moves;
+}
 
 export async function solve() {
-  const input = await parseInput();
-
-  return "Not implemented.";
+  const { graph, start } = await parseInput();
+  const travelMoves = followPipe(graph, start);
+  return travelMoves / 2;
 }
