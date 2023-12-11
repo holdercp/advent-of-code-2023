9eb399ab31b6a5549dab7d4afebf70a13a842b5d
Chris Holder <chris@chrisholder.dev>
2023-12-11 15:23:26 -0600

    Solve day 11, part 1

--- solutions/11/lib.ts
+++ solutions/11/lib.ts
@@ -1,7 +1,51 @@
 import { readInput } from "../../lib/helpers";
 
-export async function parseInput() {
-  const input = await readInput(import.meta.dir, true);
+type Universe = string[][];
 
-  return input;
+function expand(s: string): Universe {
+  const expanded: string[][] = [];
+  const lines = s.split("\n").map((l) => l.split(""));
+
+  lines.forEach((line) => {
+    if (line.every((el) => el === ".")) {
+      expanded.push([...line]);
+    }
+    expanded.push([...line]);
+  });
+
+  let offset = 0;
+  for (let i = 0; i < lines[0].length; i++) {
+    const col = lines.map((l) => l[i]);
+    if (col.every((el) => el === ".")) {
+      expanded.forEach((l) => {
+        l.splice(i + offset, 0, ".");
+      });
+      offset++;
+    }
+  }
+
+  return expanded;
+}
+
+function findGalaxies(u: Universe) {
+  const g = [];
+
+  for (let y = 0; y < u.length; y++) {
+    for (let x = 0; x < u[y].length; x++) {
+      const el = u[y][x];
+      if (el === "#") {
+        g.push([x, y]);
+      }
+    }
+  }
+  return g;
+}
+
+export async function parseInput() {
+  const input = await readInput(import.meta.dir);
+  const expanded = expand(input);
+
+  const galaxies = findGalaxies(expanded);
+
+  return { expanded, galaxies };
 }
--- solutions/11/part-1.ts
+++ solutions/11/part-1.ts
@@ -1,7 +1,18 @@
 import { parseInput } from "./lib";
 
 export async function solve() {
-  const input = await parseInput();
+  const { galaxies } = await parseInput();
 
-  return "Not implemented.";
+  let total = 0;
+  let galaxy;
+  while ((galaxy = galaxies.pop())) {
+    const [x1, y1] = galaxy;
+    total = galaxies.reduce((sum, other) => {
+      const [x2, y2] = other;
+      const distance = Math.abs(x2 - x1) + Math.abs(y2 - y1);
+      return sum + distance;
+    }, total);
+  }
+
+  return total;
 }
