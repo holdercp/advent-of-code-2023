5c3df94c00af654f69692bdf00615035ee1fcd1c
Chris Holder <chris@chrisholder.dev>
2024-04-24 08:51:00 -0500

    WIP: more progress on day 12 refactor

--- solutions/12/part-1.ts
+++ solutions/12/part-1.ts
@@ -5,73 +5,77 @@ function sumArrangementBranches(
   sizes: ConditionRecord["sizes"],
 ): number {
   return (
-    countValidArrangements(`#${list}`, [...sizes]) +
-    countValidArrangements(list, [...sizes])
+    countValidArrangements(`#${list}`, sizes) +
+    countValidArrangements(list, sizes)
   );
 }
 
-function countDamagedSpringsWithSizes(
+function countDamagedSpringsAndMatchWithSizes(
   list: ConditionRecord["list"],
   sizes: ConditionRecord["sizes"],
 ): number {
-  let targetSize = sizes.shift();
-  let damagedCount = 1;
+  const [targetSize, ...remainingSizes] = sizes;
+  let damagedCount = 0;
 
   for (let i = 0; i < list.length; i++) {
-    if (targetSize === undefined || damagedCount > targetSize) return 0;
-
-    if (list.length === 0) {
-      if (damagedCount === targetSize && sizes.length === 0) {
-        return 1;
-      } else {
-        return 0;
-      }
-    }
     const spring = list[i] as Condition;
 
     if (spring === Condition.UNKNOWN) {
+      if (damagedCount === targetSize) {
+        // Continue counting arrangements with a contiguous damaged section accounted for
+        const remainingList = list.slice(i + 1);
+        return countValidArrangements(remainingList, remainingSizes);
+      }
+
       if (damagedCount < targetSize) {
+        // Consider the UNKNOWN as DAMAGED and continue building the contiguous damaged section
         damagedCount++;
         continue;
-      } else if (damagedCount === targetSize) {
-        return countValidArrangements(list.slice(i + 1), [...sizes]);
       }
+
+      // Damaged count is greater than the target size, meaning this branch isn't valid
+      return 0;
     }
 
     if (spring === Condition.OPERATIONAL) {
       if (damagedCount === targetSize) {
-        targetSize = sizes.shift();
-        damagedCount = 0;
-        continue;
+        // We've matched a contiguous damaged section so reset and match against the next size
+        const remainingList = list.slice(i + 1);
+        return countValidArrangements(remainingList, remainingSizes);
       } else {
+        // We've broken the contiguous damaged section, so if it doesn't match the target size this branch isn't valid
         return 0;
       }
     }
 
+    // Spring is damaged; keep building contiguous section
     damagedCount++;
   }
 
-  return 0;
+  return damagedCount === targetSize ? 1 : 0;
 }
 
 function countValidArrangements(
   list: ConditionRecord["list"],
   sizes: ConditionRecord["sizes"],
 ): number {
-  const _sizes = [...sizes];
+  const sizesCopy = [...sizes];
   const firstSpring = list[0] as Condition;
   const listWithoutFirstSpring = list.slice(1);
 
+  // TODO: Handle edge cases around when list and sizes are empty
+  if (firstSpring === undefined) return 0;
+
   if (firstSpring === Condition.UNKNOWN) {
-    return sumArrangementBranches(listWithoutFirstSpring, _sizes);
+    return sumArrangementBranches(listWithoutFirstSpring, sizesCopy);
   }
 
   if (firstSpring === Condition.OPERATIONAL) {
-    return countValidArrangements(listWithoutFirstSpring, _sizes);
+    return countValidArrangements(listWithoutFirstSpring, sizesCopy);
   }
 
   if (firstSpring === Condition.DAMAGED) {
-    return countDamagedSpringsWithSizes(listWithoutFirstSpring, _sizes);
+    return countDamagedSpringsAndMatchWithSizes(list, sizesCopy);
   }
 
   throw new Error(
