d7eac9a3dafdae61e2b7913ce833a8db328d624b
Chris Holder <chris@chrisholder.dev>
2024-04-28 15:26:08 -0500

    Attemp 1 at day 13, part 1

--- solutions/13/lib.ts
+++ solutions/13/lib.ts
@@ -1,7 +1,35 @@
 import { readInput } from "../../lib/helpers";
 
-export async function parseInput() {
-  const input = await readInput(import.meta.dir);
+export type Pattern = {
+  rows: string[];
+  cols: string[];
+};
 
-  return input;
+function patternToRowsAndColsReducer(
+  rowsAndCols: Pattern,
+  pattern: string,
+  rowIndex: number,
+): Pattern {
+  pattern.split("").forEach((valleyItem, colIndex) => {
+    const { rows, cols } = rowsAndCols;
+    rows[rowIndex] = (rows[rowIndex] ?? "") + valleyItem;
+    cols[colIndex] = (cols[colIndex] ?? "") + valleyItem;
+  });
+
+  return rowsAndCols;
+}
+
+function rawToPatternMapper(raw: string): Pattern {
+  return raw.split("\n").reduce(patternToRowsAndColsReducer, {
+    rows: [],
+    cols: [],
+  } as Pattern);
+}
+
+export async function parseInput(): Promise<Pattern[]> {
+  const input = await readInput(import.meta.dir);
+  const patternsRaw = input.split("\n\n");
+  const patterns: Pattern[] = patternsRaw.map(rawToPatternMapper);
+
+  return patterns;
 }
--- solutions/13/part-1.ts
+++ solutions/13/part-1.ts
@@ -1,7 +1,101 @@
-import { parseInput } from "./lib";
+import { Pattern, parseInput } from "./lib";
 
-export async function solve() {
-  const input = await parseInput();
+type Reflection = {
+  index: number;
+  type: "horizontal" | "vertical";
+};
 
-  return "Not implemented.";
+type RelfectionIndexes = Record<string, number>;
+
+function addReflectionIndexes(
+  line: string,
+  indexes: RelfectionIndexes,
+): RelfectionIndexes {
+  const start = 0;
+  const end = line.length - 1;
+
+  let isComplete = false;
+  let walkDown = end - 1;
+  let walkUp = 1;
+
+  while (!isComplete) {
+    if (walkDown === 1 || walkUp === end - 1) {
+      isComplete = true;
+    }
+
+    const walkUpMid = line.length - Math.ceil((end - walkUp) / 2);
+    const walkDownMid = Math.ceil((walkDown - start) / 2);
+    const walkUpSlices = [
+      line.slice(walkUp, walkUpMid),
+      line.slice(walkUpMid, line.length).split("").reverse().join(""),
+    ];
+    const walkDownSlices = [
+      line.slice(start, walkDownMid),
+      line
+        .slice(walkDownMid, walkDown + 1)
+        .split("")
+        .reverse()
+        .join(""),
+    ];
+
+    if (walkUpSlices[0] === walkUpSlices[1]) {
+      indexes[walkUpMid - 1] = (indexes[walkUpMid - 1] ?? 0) + 1;
+    }
+
+    if (walkDownSlices[0] === walkDownSlices[1]) {
+      indexes[walkDownMid] = (indexes[walkDownMid] ?? 0) + 1;
+    }
+
+    walkDown -= 2;
+    walkUp += 2;
+  }
+
+  return indexes;
+}
+
+function getReflectionIndex(
+  pattern: Pattern["rows"] | Pattern["cols"],
+): number | undefined {
+  const indexes: RelfectionIndexes = {};
+
+  const allIndexes = pattern.reduce((indexes, line) => {
+    return addReflectionIndexes(line, indexes);
+  }, indexes);
+
+  const commonIndex = Object.entries(allIndexes).find(
+    ([, count]) => count === pattern.length,
+  );
+
+  return !commonIndex ? undefined : Number(commonIndex[0]);
+}
+
+function calcReflectionTotal(reflection: Reflection): number {
+  return reflection.type === "horizontal"
+    ? (reflection.index + 1) * 100
+    : reflection.index + 1;
+}
+
+function sumPatternsReducer(sum: number, { rows, cols }: Pattern): number {
+  let reflection: Reflection | undefined;
+  let reflectionIndex = getReflectionIndex(rows);
+  if (reflectionIndex !== undefined) {
+    reflection = { index: reflectionIndex, type: "vertical" };
+  } else {
+    reflectionIndex = getReflectionIndex(cols);
+    reflection = reflectionIndex
+      ? { index: reflectionIndex, type: "horizontal" }
+      : undefined;
+  }
+
+  if (!reflection) throw new Error("No reflection found!");
+
+  const reflectionTotal = calcReflectionTotal(reflection);
+
+  return sum + reflectionTotal;
+}
+
+export async function solve(): Promise<number> {
+  const patterns = await parseInput();
+
+  return patterns.reduce(sumPatternsReducer, 0);
 }
